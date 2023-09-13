e77cf3866e5fc030e63067059938d0f32d44d2a2
saattaja <janita.elisa@outlook.com>
2023-09-13 14:43:01 +0300

    toimi nyt

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"
+      backgroundColor='cyan' />
     </View>
   );
 }
@@ -14,7 +16,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
   },
 });
